c67b938f8f1153859c3d7a01fc04f42cff828344
Julio-1993 <julioandrade1993@gmail.com>
2025-02-10 08:23:17 -0300

    manha1002

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -5,7 +5,7 @@
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-@set "VIRTUAL_ENV=C:\Users\JulioCA\OneDrive\Documentos\PythonDev\venv"
+@set "VIRTUAL_ENV=C:\Users\JulioCA\PythonDev\venv"
 
 @set "VIRTUAL_ENV_PROMPT="
 @if NOT DEFINED VIRTUAL_ENV_PROMPT (
